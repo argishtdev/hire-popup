dc534cb3b92a77ea7960aa34461d7d37c4f7e50c
Arman <armani.developer@gmail.com>
2019-09-20 17:43:47 +0400

    Initial setup

---
+++ src/components/Modal.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="modal-component">
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <Modal />
+  </div>
+</template>
+
+<script>
+import Modal from '@/components/Modal.vue'
+
+export default {
+  name: 'home',
+  components: {
+    Modal
+  }
+}
+</script>
